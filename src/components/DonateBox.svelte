dd920b615547bb46eaf347a42e4ddc547dccb515
Micheal Parks <michealparks1989@gmail.com>
2025-01-08 20:50:26 -0500

    donate page

---
+++ src/components/DonateBox.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<div class="flex flex-wrap gap-3">
+  {#each { length: 1000 }, index}
+    <button class="bg-[#006AF9] w-[5.4rem] py-2 text-white text-xs text-center">
+      ${(index + 1) ** 2}.00
+    </button>
+  {/each}
+</div>
