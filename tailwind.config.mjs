31102459c50151dffc3304353ec9330e53c2e4b0
Jon Franco <jonfranco@Jons-Mac-mini.local>
2025-01-11 19:29:29 -0500

    working on the design for the article page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+    screens: {
+      sm: '600px',
+      md: '850px',
+      lg: '960px',
+      xl: '1140px',
+      '2xl': '1220px', // Capping at 2xl
+    },
     extend: {
       borderColor: {
         DEFAULT: '#9C9793', // Replace this with your desired default border color
+      },
+    },
+    container: {
+      screens: {
+        sm: '600px',
+        md: '825px',
+        lg: '960px',
+        xl: '1140px',
+        '2xl': '1220px', // Capping at 2xl
       },
     },
   },
